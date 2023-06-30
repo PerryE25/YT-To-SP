16f29b4f8b21be4305b9f52134622549695fbc54
PerryE25 <93444097+PerryE25@users.noreply.github.com>
2023-06-29 21:16:26 -0500

    Add files via upload

---
+++ secret.py
@@ -0,0 +1,8 @@
+# For the YouTube Data API
+PLAYLIST_ID = "PLqqrBkAAeqhNGlRf3x2Cp1qDO1Q1MngZK"
+
+# For the Spotify Web API
+CLIENT_ID = "17730618f7f54fc1bf3a1e2ba1f9f7a5"
+CLIENT_SECRET = "5861882f224c4d8f87dfcaece41ec31d"
+USER_ID = "31rl4azkpx7rssgixhqvan2voai4"
+REDIRECT_URI = "https://www.google.com"
